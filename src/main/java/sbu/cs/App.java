31aa23beb6b462ca77fa99337f62bb249b999e76
Muhammad Karbalaee Shabani <muhammad.ksht@gmail.com>
2021-04-21 20:38:47 +0430

    process function is implemented

--- src/main/java/sbu/cs/App.java
+++ src/main/java/sbu/cs/App.java
@@ -5,14 +5,91 @@ public class App
     private Block[][] blocks;
     private int[][] arr;
     private int n;
+    private String input;
 
     public String main(int n, int[][] arr, String input)
     {
         blocks = new Block[n][n];
         this.arr = arr;
         this.n = n;
+        this.input = input;
+
         initialize();
-        return null;
+
+        return process();
+    }
+
+    private String process()
+    {
+        firstProcessor();
+        middleRowsProcessor();
+        return lastRowProcessor();
+    }
+
+    private String lastRowProcessor()
+    {
+        ((PinkBlock) blocks[n - 1][1]).setInput2(((GreenBlock) blocks[0][0]).getOutput());
+        for (int i = 1; i < n - 1; i++)
+        {
+            ((PinkBlock)blocks[n - 1][i + 1]).setInput2(((PinkBlock)blocks[n - 1][i]).getOutput());
+        }
+        return ((PinkBlock)blocks[n - 1][n - 1]).getOutput();
+    }
+
+    private void middleRowsProcessor()
+    {
+        for (int i = 1; i < n - 1; i++)
+        {
+            if (i == n - 2)
+            {
+                ((YellowBlock) blocks[i + 1][0]).setInput(((GreenBlock) blocks[i][0]).getOutput());
+            }
+            else
+            {
+                ((GreenBlock) blocks[i + 1][0]).setInput(((GreenBlock) blocks[i][0]).getOutput());
+            }
+            ((BlueBlock) blocks[i][1]).setLeftInput(((GreenBlock) blocks[i][0]).getOutput());
+            for (int j = 1; j < n - 1; j++)
+            {
+                if (i == n - 2)
+                {
+                    ((PinkBlock) blocks[i + 1][j]).setInput1(((BlueBlock) blocks[i][j]).getDownOutput());
+                }
+                else
+                {
+                    ((BlueBlock) blocks[i + 1][j]).setUpInput(((BlueBlock) blocks[i][j]).getDownOutput());
+                }
+                if (j == n - 2)
+                {
+                    ((PinkBlock) blocks[i][j + 1]).setInput2(((BlueBlock) blocks[i][j]).getDownOutput());
+                }
+                else
+                {
+                    ((BlueBlock) blocks[i][j + 1]).setUpInput(((BlueBlock) blocks[i][j]).getDownOutput());
+                }
+            }
+            ((PinkBlock) blocks[i + 1][n - 1]).setInput1(((PinkBlock) blocks[i][n - 1]).getOutput());
+        }
+    }
+
+    private void firstProcessor()
+    {
+        ((GreenBlock) blocks[0][0]).setInput(input);
+        ((GreenBlock) blocks[1][0]).setInput(((GreenBlock) blocks[0][0]).getOutput());
+        ((GreenBlock) blocks[0][1]).setInput(((GreenBlock) blocks[0][0]).getOutput());
+        for (int i = 1; i < n - 1; i++)
+        {
+            ((BlueBlock) blocks[1][i]).setUpInput(((GreenBlock) blocks[0][i]).getOutput());
+            if (i == n - 2)
+            {
+                ((YellowBlock) blocks[0][i + 1]).setInput(((GreenBlock) blocks[0][i]).getOutput());
+            }
+            else
+            {
+                ((GreenBlock) blocks[0][i + 1]).setInput(((GreenBlock) blocks[0][i]).getOutput());
+            }
+        }
+        ((PinkBlock) blocks[1][n - 1]).setInput1(((YellowBlock) blocks[0][n - 1]).getOutput());
     }
 
     private BlackFunction blackFunctionMaker(int functionNumber)
